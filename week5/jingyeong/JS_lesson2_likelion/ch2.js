43b9c96ad80e5adefc6fb0f0f6ce867d49e55ea4
catherine <68558189+jg980404@users.noreply.github.com>
2021-05-18 22:25:33 +0900

    Add files via upload

---
+++ week5/jingyeong/JS_lesson2_likelion/ch2.js
@@ -0,0 +1,24 @@
+class Animal {
+    constructor(leg) {
+        this.leg = leg
+    }
+
+    printAnimal() {
+        console.log(this.name+"은 "+String(this.leg)+"개의 다리를 가진다.")
+    }
+}
+
+// Lion -> Animal 유용한 기능들 가져다 쓰기!
+class Lion extends Animal {
+    constructor(name, leg){
+        super(leg) // 부모의 input (constructor의 인풋)
+        this.name = name
+    }
+    getName() {
+        console.log("내 이름은 "+this.name)
+    }
+}
+
+myLion = new Lion("King",4)
+myLion.getName()
+myLion.printAnimal()
